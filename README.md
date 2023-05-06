48c2b589c467262cc26798e1e19e563934fb88e2
iwan.roberts <iwan.roberts@defra.onmicrosoft.com>
2023-05-06 11:54:20 +0100

    added a jest test for the products GET call

--- README.md
+++ README.md
@@ -29,7 +29,7 @@
 ## Installation
 
 ```bash
-$ npm install
+npm install
 ```
 
 ## Running the app
@@ -61,6 +61,8 @@ $ npm run test:cov
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
+
+Got the information to build the docker file [from here](https://www.tomray.dev/nestjs-docker-production)
 
 ## Stay in touch
 
---
+++ test/products.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('ProductsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('products/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/products')
+      .expect(200)
+      .expect('[]');
+  });
+});
